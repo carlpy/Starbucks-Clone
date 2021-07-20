83572b13bb5f2510818545b0547de8ff59d58dc7
Carlos_Z <carlosmariozambrano2015@gmail.com>
2021-07-19 23:07:50 -0500

    segunda navegacion del menu agregada :)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
-// Creating the elements
+///------ Creating the elements ------///
+
+// Navbar
+const navbarContainer = document.querySelector('.nav__mobile');
+
+// First navbar
 
 const burgerButton = document.querySelector('.nav__burger');
 const mobileMenu = document.querySelector('.nav__mobile-menu');
 const mobileBlackScreen = document.querySelector('nav__mobile-black-screen');
 
+// Second navbar after click on menu button
+const menuButton = document.querySelector('.btn-menu');
+const backMenuButton = document.querySelector('.nav__back-btn');
+const coffeeMenu = document.querySelector('.nav__mobile-menu2');
+
+///---- Events ----///
+
 burgerButton.addEventListener('click', () => {
+    coffeeMenu.classList.remove('show-menu');
     mobileMenu.classList.toggle('show-menu');
 });
+
+menuButton.addEventListener('click', () => {
+    coffeeMenu.classList.toggle('show-menu');
+});
+
+backMenuButton.addEventListener('click', () => {
+    coffeeMenu.classList.toggle('show-menu');
+});
+
+
